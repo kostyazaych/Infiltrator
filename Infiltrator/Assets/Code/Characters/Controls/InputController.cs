691c984c06a9086ba920fc578e23abcec868911d
kostyazaych <kostya.from.rostov@gmail.com>
2021-01-16 21:22:29 +0300

    Movement

--- Infiltrator/Assets/Code/Characters/Controls/InputController.cs
+++ Infiltrator/Assets/Code/Characters/Controls/InputController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class InputController : MonoBehaviour
 {
@@ -10,7 +13,8 @@ public class InputController : MonoBehaviour
     private float movementSpeed = 3f;
     private float readyMovementSpeed = 1.5f;
     private float currentSpeed = 0f;
-    private float speedSmoothVelocity = 0f;
+    [HideInInspector] public float speedBuffCoef = 1f;
+    private float speedSmoothVelocity = 0.05f;
     private float speedSmoothTime = 0.01f;
     private Rigidbody RootRigidbody;
     private float jumpHeight = 1.0f;
@@ -27,8 +31,8 @@ public class InputController : MonoBehaviour
     private Transform player;
     private Transform mainCameraTransform = null;
 
-    private bool ReadyWeaponMovement = false;
-
+    private bool ReadyWeaponMovement = true;
+    private Vector3 rotation;
 
     // Start is called before the first frame update
     void Awake()
@@ -53,8 +57,12 @@ public class InputController : MonoBehaviour
             Jump();
 
         //ReadyWeapon();
-        // RotateCharacter();
-
+         RotateCharacter();
+        
+        /*this.rotation = new Vector3(0, Input.GetAxisRaw("Mouse X") * mouseSense * Time.deltaTime, 0);
+        this.transform.Rotate(this.rotation);
+        */
+        
         //Attacking
         /* if (Input.GetButton("Fire1"))
              GetComponent<CharacterBase>().CharacterAttack();
@@ -66,21 +74,11 @@ public class InputController : MonoBehaviour
     {
         Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
-
-
-        forward.y = 0;
-        right.y = 0;
-
-        forward.Normalize();
-        right.Normalize();
-
-
+        movementInput.x = movementInput.x * -1;
         //Инвертирую направление движения, * -1 нужно, чтоб на "S" двигался вниз, а не вверх
-       // Vector3 desiredMoveDirection = (right * movementInput.y * -1 +  forward * movementInput.x ).normalized;
-        Vector3 desiredMoveDirection = (right * movementInput.y  +  forward * movementInput.x ).normalized;
-        Debug.Log("desiredMoveDirection: " + desiredMoveDirection);
+
+        Vector3 desiredMoveDirection = new Vector3(movementInput.y, 0, movementInput.x);
+        desiredMoveDirection.Normalize();
         //transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
         Vector3 gravityVector = Vector3.zero;
       
@@ -89,18 +87,18 @@ public class InputController : MonoBehaviour
         {
             gravityVector.y -= gravity;
         }
-
-        float targetSpeed = 0;
+        Vector3 kekVec = new Vector3(1f, 0f, 1f);
+        float targetSpeed;
         if (ReadyWeaponMovement == false)
         {
-            targetSpeed = movementSpeed * movementInput.magnitude;
+            targetSpeed = movementSpeed * desiredMoveDirection.magnitude;     
         }
         else
         {
-            targetSpeed = readyMovementSpeed * movementInput.magnitude;
+            targetSpeed = readyMovementSpeed * desiredMoveDirection.magnitude;
         }
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);     
         controller.Move(desiredMoveDirection * currentSpeed * Time.deltaTime);
         //controller.Move(gravityVector * Time.deltaTime);
     }
@@ -113,16 +111,22 @@ public class InputController : MonoBehaviour
 Debug.Log("JUMP");
     }
 
-    /* void RotateCharacter()
+   private void RotateCharacter()
      {
          float mouseX = Input.GetAxis("Mouse X");
          float mouseY = Input.GetAxis("Mouse Y");
          float mouseSenseReady = mouseSense * 0.5f;
          float rotAmountX = 0f;
          float rotAmountY = 0f;
+         Vector3 rotation;
+    
+         transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime, 0);
+      
 
-
-         if (ReadyWeaponMovement == false)
+         /* this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * mouseSense * Time.deltaTime, 0);
+          player.transform.Rotate(this.rotation123);   
+           */
+         /*if (ReadyWeaponMovement == false)
          {
              rotAmountX = mouseX * mouseSense;
              rotAmountY = mouseY * (mouseSense * -1.4f);
@@ -130,7 +134,7 @@ Debug.Log("JUMP");
          else
          {
              rotAmountX = mouseX * mouseSenseReady;
-             rotAmountY = mouseY * (mouseSenseReady * -1.4f);
+             rotAmountY = mouseY; //* (mouseSenseReady * -1.4f);
          }
 
            Vector3 rotPlayer = player.transform.rotation.eulerAngles;
@@ -151,20 +155,20 @@ Debug.Log("JUMP");
                    rotPlayer.x = 55f;
                }
            }
-           else
+            else
            {
                rotPlayer.x = 0;
            }
 
-           if (Input.GetButtonUp("Turn"))
+          if (Input.GetButtonUp("Turn"))
                rotPlayer.y = rotPlayer.y - 180f;
 
-           player.rotation = Quaternion.Euler(rotPlayer);
+           player.rotation = Quaternion.Euler(rotPlayer);*/
 
 
        }
 
-     */
+     
 
 
     /*void ReadyWeapon()
